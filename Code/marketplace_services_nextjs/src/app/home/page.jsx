0ba2d8c03b2a20239a5582c47c62477f4ec7b55b
Miguel Petherson <pethersonzada@gmail.com>
2025-11-18 20:07:15 -0300

    Modificando estrutura dos componentes

---
+++ Code/marketplace_services_nextjs/src/app/home/page.jsx
@@ -0,0 +1,9 @@
+import Title from "@/components/home/Title";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Title />
+    </div>
+  );
+}
---
+++ Code/marketplace_services_nextjs/src/components/home/Title.jsx
@@ -0,0 +1,3 @@
+export default function Title() {
+  return <h1>Bem-vindo à Nossa Página Inicial</h1>;
+}
