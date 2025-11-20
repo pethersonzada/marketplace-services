05203738f16fb3e8330862c913898ca5a870bcbf
Miguel Petherson <pethersonzada@gmail.com>
2025-11-20 20:25:32 -0300

    Upload cards de perfil

--- Code/marketplace_services_nextjs/src/app/home/page.jsx
+++ Code/marketplace_services_nextjs/src/app/home/page.jsx
@@ -3,6 +3,7 @@ import Coupon from "@/components/home/Coupon";
 import Main from "@/components/home/Main";
 import Footer from "@/components/ui/Footer";
 import Services from "@/components/home/Services";
+import Avatar from "@/components/ui/Avatar";
 
 export default function HomePage() {
   return (
@@ -10,21 +11,19 @@ export default function HomePage() {
       <Title />
 
       <Coupon />
-
+      
       <Main />
-
+      
       <Services />
-
-
+      
       <div className = "how-it-works-container">
-
         <h2>Como Funciona?</h2>
-
         <img src="/banner/how-it-works.png" alt="" />
       </div>
 
+      <Avatar />
+      
       <Footer />
-
     </div>
   );
 }
---
+++ Code/marketplace_services_nextjs/src/components/ui/Avatar.jsx
@@ -0,0 +1,104 @@
+export default function Avatar() {
+    return <div>
+  
+    <div className = "how-it-works-container">
+        <h2>Conheça nossos principais prestadores de serviço!</h2>
+    </div>
+
+    <div className = "profile-card-container">
+
+        <div className = "profile-card">
+
+            <div className="left-side">
+                <img src="/avatar/1.png" alt="Avatar" />
+                <h3>Snoop Dogg</h3>
+                <p className = "left-side-func">Animador de Festas</p>
+
+            </div>
+
+            <div className="right-side">
+
+                <div className="info-block">
+                    <p className="number">257</p>
+                    <p className="label">negócios fechados</p>
+                </div>
+
+                <div className="info-block">
+                    <p className="number-39">4,4/5 ⭐</p>
+                    <p className="label">estrelas</p>
+                </div>
+
+                <div className="info-block">
+                    <p className="number">2</p>
+                    <p className="label">anos realizando serviços</p>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div className = "profile-card">
+
+            <div className="left-side">
+                <img src="/avatar/10.png" alt="Avatar" />
+                <h3>Walter White</h3>
+                <p className = "left-side-func">Químico</p>
+
+            </div>
+
+            <div className="right-side">
+
+                <div className="info-block">
+                    <p className="number">4.562</p>
+                    <p className="label">negócios fechados</p>
+                </div>
+
+                <div className="info-block">
+                    <p className="number-39">4,9/5 ⭐</p>
+                    <p className="label">estrelas</p>
+                </div>
+
+                <div className="info-block">
+                    <p className="number">7</p>
+                    <p className="label">anos realizando serviços</p>
+                </div>
+
+            </div>
+            
+        </div>
+
+        <div className = "profile-card">
+
+            <div className="left-side">
+                <img src="/avatar/21.png" alt="Avatar" />
+                <h3>Johnny Cash</h3>
+                <p className = "left-side-func">Desenvolvedor</p>
+
+            </div>
+
+            <div className="right-side">
+
+                <div className="info-block">
+                    <p className="number">17.568</p>
+                    <p className="label">negócios fechados</p>
+                </div>
+
+                <div className="info-block">
+                    <p className="number-39">5/5 ⭐</p>
+                    <p className="label">estrelas</p>
+                </div>
+
+                <div className="info-block">
+                    <p className="number">22</p>
+                    <p className="label">anos realizando serviços</p>
+                </div>
+
+            </div>
+            
+        </div>
+
+    </div>
+    
+</div>
+
+}
