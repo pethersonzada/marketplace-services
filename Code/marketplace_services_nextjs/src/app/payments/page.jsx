8f498eee5ea5178443b5b2a1ddfe7a0242af5d97
yallerivika <yalle687@gmail.com>
2025-11-27 20:13:12 -0300

    Upload página de pagamentos

---
+++ Code/marketplace_services_nextjs/src/app/payments/page.jsx
@@ -0,0 +1,23 @@
+import PaymentsForm from "@/components/payments/paymentsForm";
+import Footer from "@/components/ui/Footer";
+import OrderSummary from "@/components/payments/orderSummary";
+import '@/styles/paymentsPage.css';
+
+export default function Payment() {
+  return (
+    <main>
+       <div className="left_right">
+
+        <div className="left">
+            <PaymentsForm />
+        </div>
+        <div className="right">
+            <OrderSummary/>
+        </div>
+       </div>
+       
+       
+       <Footer />
+    </main>
+  );
+}
