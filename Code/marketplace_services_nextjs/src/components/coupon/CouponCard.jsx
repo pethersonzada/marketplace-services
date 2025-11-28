324c72625a9d6f1790afd88ce492231acb9839f5
Rafilbx <rafilbx@gmail.com>
2025-11-28 12:10:42 -0300

    Upload página de cupons

--- Code/marketplace_services_nextjs/src/components/coupon/CouponCard.jsx
+++ Code/marketplace_services_nextjs/src/components/coupon/CouponCard.jsx
@@ -3,34 +3,186 @@ import "../../styles/global.css";
 
 export default function CouponCard() {
     return <div className="coupon-items">
-        <div className="coupon-card">
+        
+            <div className="coupon-card">
 
-            <div className="coupon-title">
+                <div className="coupon-title">
 
-                <img src="/icons/desconto.png" alt="" />
-                <h4>R$15 Off em Qualquer coisa!</h4>
+                    <img src="/icons/desconto.png" alt="" />
+                    <h4>R$15 Off em Qualquer coisa!</h4>
+
+                </div>
+
+                <div className="coupon-main-text">
+
+                    <p>Valor mínimo de R$19,99 em qualquer serviço.</p>
+
+                </div>
+
+                <div className="coupon-main-content">
+
+                    <button className="all-buttons">Ver Lojas</button>
+                    <p>Sem tempo definido</p>
+
+                </div>
+
+                <div className="coupon-footer">
+
+                    <p>O cupom será aplicado no pagamento</p>
+
+                </div>
 
             </div>
 
-            <div className="coupon-main-text">
+            <div className="coupon-card">
 
-                <p>Cupom de boas vindas! Valor mínimo de R$19,99 em qualquer serviço.</p>
+                <div className="coupon-title">
+
+                    <img src="/icons/desconto.png" alt="" />
+                    <h4>R$20 Off em Serviços domésticos!</h4>
+
+                </div>
+
+                <div className="coupon-main-text">
+
+                    <p>Valor mínimo de R$49,99 para serviços domésticos.</p>
+
+                </div>
+
+                <div className="coupon-main-content">
+
+                    <button className="all-buttons">Ver Lojas</button>
+                    <p>Até dia 02/12/2025</p>
+
+                </div>
+
+                <div className="coupon-footer">
+
+                    <p>O cupom será aplicado no pagamento</p>
+
+                </div>
 
             </div>
 
-            <div className="coupon-main-content">
+            <div className="coupon-card">
 
-                <button className="all-buttons">Ver Lojas</button>
-                <p>Sem tempo definido</p>
+                <div className="coupon-title">
+
+                    <img src="/icons/desconto.png" alt="" />
+                    <h4>R$500 Off em Serviços no carrinho!</h4>
+
+                </div>
+
+                <div className="coupon-main-text">
+
+                    <p>Valor mínimo de R$949,99 em qualquer serviço no carrinho.</p>
+
+                </div>
+
+                <div className="coupon-main-content">
+
+                    <button className="all-buttons">Ver Lojas</button>
+                    <p>Até dia 24/12/2026</p>
+
+                </div>
+
+                <div className="coupon-footer">
+
+                    <p>O cupom será aplicado no pagamento</p>
+
+                </div>
 
             </div>
 
-            <div className="coupon-footer">
+            <div className="coupon-card">
 
-                <p>O cupom será aplicado no pagamento</p>
+                <div className="coupon-title">
+
+                    <img src="/icons/desconto.png" alt="" />
+                    <h4>R$12 Off para Informática!</h4>
+
+                </div>
+
+                <div className="coupon-main-text">
+
+                    <p>Valor mínimo de R$9,99 em serviços de informática.</p>
+
+                </div>
+
+                <div className="coupon-main-content">
+
+                    <button className="all-buttons">Ver Lojas</button>
+                    <p>Até dia 29/02/2026</p>
+
+                </div>
+
+                <div className="coupon-footer">
+
+                    <p>O cupom será aplicado no pagamento</p>
+
+                </div>
 
             </div>
 
-        </div>
+            <div className="coupon-card">
+
+                <div className="coupon-title">
+
+                    <img src="/icons/desconto.png" alt="" />
+                    <h4>R$100% Off para Chaveiros!</h4>
+
+                </div>
+
+                <div className="coupon-main-text">
+
+                    <p>Valor mínimo de R$0,00 em serviços de chaveiro.</p>
+
+                </div>
+
+                <div className="coupon-main-content">
+
+                    <button className="all-buttons">Ver Lojas</button>
+                    <p>Sem tempo definido</p>
+
+                </div>
+
+                <div className="coupon-footer">
+
+                    <p>O cupom será aplicado no pagamento</p>
+
+                </div>
+
+            </div>
+
+            <div className="coupon-card">
+
+                <div className="coupon-title">
+
+                    <img src="/icons/desconto.png" alt="" />
+                    <h4>R$49,99 Off na primeira faxina!</h4>
+
+                </div>
+
+                <div className="coupon-main-text">
+
+                    <p>Valor mínimo de R$59,99 em serviços de faxina.</p>
+
+                </div>
+
+                <div className="coupon-main-content">
+
+                    <button className="all-buttons">Ver Lojas</button>
+                    <p>Sem tempo definido</p>
+
+                </div>
+
+                <div className="coupon-footer">
+
+                    <p>O cupom será aplicado no pagamento</p>
+
+                </div>
+
+            </div>
+
     </div>;
 }
