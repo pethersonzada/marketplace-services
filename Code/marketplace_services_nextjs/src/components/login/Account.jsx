bdad583ba905bc9d717e49d5b1be36e23ada0981
Rafilbx <rafilbx@gmail.com>
2025-12-07 16:29:05 -0300

    Corrigindo animações quebradas

--- Code/marketplace_services_nextjs/src/components/login/Account.jsx
+++ Code/marketplace_services_nextjs/src/components/login/Account.jsx
@@ -6,10 +6,7 @@ export default function Account() {
             <div className="account-left-side">
 
                 <div className="greeting-top">
-                    <h1>Olá, </h1>
-                    <a href="/home">
-                    <img className="login-logo" src="/logo/logo-branca.png" alt="Logo" />
-                    </a>
+                    <h1>Olá, Seja</h1>
                 </div>
 
                 <h1>Bem Vindo!</h1>
